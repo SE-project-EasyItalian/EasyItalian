d50cdbd4d5ade5b8d9a4938862d7b056046b5f2c
xsxun <xsxuno@outlook.com>
2018-04-09 17:44:24 +0800

    add wordlist.xml and the usage of greenDao

--- app/src/main/java/eipro/se/tju/easyitalian/RecitePage.java
+++ app/src/main/java/eipro/se/tju/easyitalian/RecitePage.java
@@ -1,11 +1,26 @@
 package eipro.se.tju.easyitalian;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
 public class RecitePage extends Activity {
@@ -46,6 +61,17 @@ public class RecitePage extends Activity {
             @Override
             public void onClick(View view) {
 
+                // the following two lines show the usage of getting info from database
+                // my.queryAll
+                // my.queryForId   根据 id 排序
+                // my.queryForAppearTime   根据出现次数排序
+                // my.queryForCorrectTime  根据正确次数排序
+                // my.queryForIncorrectTime  根据错误次数排序
+                DaoOpt my =DaoOpt.Companion.getInstance();
+                // Warning!!  java需要处理空指针
+                String testword = my.queryAll(getApplicationContext()).get(2).getExample();
+                //end
+
                 Toast.makeText(RecitePage.this,"nothing", Toast.LENGTH_LONG).show();
             }
         });
@@ -76,4 +102,55 @@ public class RecitePage extends Activity {
         });
     }
 
+    private Word getWordFromXml (Integer num ){
+        DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+        try{
+            DocumentBuilder dombuilder=dbf.newDocumentBuilder();
+            try {
+                Document doc = dombuilder.parse(getAssets().open("wordlist.xml"));
+                NodeList  wordList = doc.getElementsByTagName("items");
+                Word thisWord = new Word();
+                thisWord.setId(num.longValue());
+                thisWord.setAppearTime(0);
+                thisWord.setCorrectTime(0);
+                thisWord.setIncorrectTime(0);
+                if(num<wordList.getLength()){
+                    Node elem = wordList.item(num);
+                    thisWord.setWord(elem.getChildNodes().item(1).getTextContent());
+                    thisWord.setTransform(elem.getChildNodes().item(3).getTextContent());
+                    thisWord.setTranslation(elem.getChildNodes().item(5).getTextContent());
+                    thisWord.setExample(elem.getChildNodes().item(7).getTextContent());
+                }else {
+                    thisWord.setWord("None");
+                    thisWord.setTransform("None");
+                    thisWord.setTranslation("None");
+                    thisWord.setExample("None");
+                }
+                return thisWord;
+            }catch (IOException e){
+                Log.e("Error:",e.getMessage());
+            }catch (SAXException e) {
+                Log.e("Error: ", e.getMessage());
+                return null;
+            }
+            }catch (ParserConfigurationException e){
+            Log.e("Error: ", e.getMessage());
+            return null;
+        }
+
+
+    return null;
+
+    }
+
+    // call createDatabase() to create wordlist.db
+    private void createDatabase(){
+        DaoOpt my = DaoOpt.Companion.getInstance();
+        my.deleteAllData(this);
+        for(Integer i=0;i<8;i++){
+            Word insertWord = getWordFromXml(i);
+            if(insertWord==null) break;
+            my.insertData(this,insertWord);
+        }
+    }
 }
